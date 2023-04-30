9ed59afa1d1ee81596b1c595db6deb3c9b09853e
r=cos(4θ) <olvadis2004@gmail.com>
2023-04-30 14:44:28 -0500

    Se añade una clase

---
+++ Clase.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jhean carlos on 30/04/2023.
+//
+
+#include "Clase.h"
---
+++ Clase.h
@@ -0,0 +1,14 @@
+//
+// Created by Jhean carlos on 30/04/2023.
+//
+
+#ifndef HELLO_GIT_CLASE_H
+#define HELLO_GIT_CLASE_H
+
+
+class Clase {
+
+};
+
+
+#endif //HELLO_GIT_CLASE_H
